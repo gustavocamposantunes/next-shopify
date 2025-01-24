ecd3400fc7d598fd5169d995ddd1474c7e903488
Decante <gustavocamposantunes@gmail.com>
2025-01-24 17:13:16 -0300

    test: ensure endpoint is set correctly when GraphQlClientAdapter is instantiated

--- src/infra/graphql/graphql-client-adapter.spec.ts
+++ src/infra/graphql/graphql-client-adapter.spec.ts
@@ -1,10 +1,22 @@
 import { faker } from "@faker-js/faker"
 import { GraphQlClientAdapter } from "./graphql-client-adapter"
 
+type SutTypes = {
+  graphql: GraphQlClientAdapter
+}
+
+const makeSut = (endpoint = faker.internet.url()): SutTypes => {
+  const graphql = new GraphQlClientAdapter(endpoint)
+  return {
+    graphql
+  }
+}
+
 describe("GraphQlClientAdapter", () => {
   it("Should call request with the correct query", () => {
     const query = faker.lorem.sentence()
-    const graphql = new GraphQlClientAdapter()
+    
+    const { graphql } = makeSut()
 
     const requestSpy = jest.spyOn(graphql, "request");
 
@@ -13,5 +25,13 @@ describe("GraphQlClientAdapter", () => {
     expect(requestSpy).toHaveBeenCalledWith(query)
 
     requestSpy.mockRestore();
+  })
+
+  it("Should correctly set the endpoint when instantiating GraphQlClientAdapter", () => {
+    const endpoint = faker.internet.url()
+
+    const { graphql } = makeSut(endpoint)
+
+    expect(graphql.endpoint).toBe(endpoint)
   })
 })
