b6d0df0049769e258f576404028314d8fd96e4a4
Decante <gustavocamposantunes@gmail.com>
2025-01-26 14:29:10 -0300

    chore(commitlint): update rules to allow longer commit messages
    - Set  to 100 characters to permit slightly longer headers
    - Disabled line length limits for the body and footer by setting  and  to
    - These changes improve flexibility for writing detailed and descriptive commit messages

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -5,7 +5,11 @@ const Configuration: UserConfig = {
   extends: ['@commitlint/config-conventional'],
   parserPreset: 'conventional-changelog-atom',
   formatter: '@commitlint/format',
-  rules: {},
+  rules: {
+    'header-max-length': [2, 'always', 100],
+    'body-max-line-length': [0, 'always', Infinity],
+    'footer-max-line-length': [0, 'always', Infinity],
+  },
 };
 
 export default Configuration;
